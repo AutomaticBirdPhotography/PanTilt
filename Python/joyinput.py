08f340d0713b7b9eeee337828f730d0ad11e5294
Robert Brenner Marthins <04roberten@gmail.com>
2023-03-27 16:33:09 +0200

    endra hvordan navnene returneres

--- Python/joyinput.py
+++ Python/joyinput.py
@@ -1,8 +1,13 @@
 import pygame
 import time
 
-# Liste over navnene på alle knappene på kontrolleren
-BUTTON_NAMES = ['X', 'A', 'B', 'Y', 'BACK', 'UP', 'DOWN']
+BUTTON_NUMBER_NAMES = { 0 : "X", 
+                        1 : "A",
+                        2 : "B",
+                        3 : "Y",
+                        8 : "BACK"}
+HAT_NUMBER_NAMES = { 1 : "UP",
+                    -1 : "DOWN"}
 
 class Controller:
     def __init__(self, index: int = 0):
@@ -11,9 +16,6 @@ class Controller:
         """
         self.index = index
         self.is_disconnected = False
-        # Dictionary som holder oversikt over hvilke knapper som er aktive
-        # (True) eller ikke (False)
-        self.active_buttons = {button: False for button in BUTTON_NAMES}
         self.joystick = None
         # Initialiserer kontrolleren
         self.check_controller()
@@ -26,19 +28,24 @@ class Controller:
         pygame.joystick.init()
         while True:
             joystick_count = pygame.joystick.get_count()
+
             if joystick_count > 0:
                 self.joystick = pygame.joystick.Joystick(self.index)
                 self.joystick.init()
                 print(f"Kontroller [{+self.joystick.get_name()}] ble koblet til")
                 self.is_disconnected = False
                 break
-            else:
-                # Hvis ingen kontroller er tilkoblet, venter den i ett sekund
-                # og skriver ut en melding hvis det ikke allerede er gjort
+
+            if not joystick_count:
                 if not self.is_disconnected:
                     print("Koble til kontroller!")
                     self.is_disconnected = True
                 time.sleep(1)
+                self.check_controller
+
+            else:
+                self.is_disconnected = False
+            
 
     def get_joystick_position(self, knob_number: int = 0, value_factor: float = 1) -> str:
         """
@@ -48,6 +55,9 @@ class Controller:
         `value_factor`.
         """
         try:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.stop()
             x = round(self.apply_deadzone(self.joystick.get_axis((knob_number+1)*2-2)*100)*value_factor)
             y = round(self.apply_deadzone(self.joystick.get_axis((knob_number+1)*2-1)*100)*value_factor)
             return f"{x:03d},{y:03d}"
@@ -63,24 +73,14 @@ class Controller:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.stop()
-        for i in range(4):
+        for i in self.joystick.get_numbuttons():
             if (self.joystick.get_button(i) == 1):
-                return BUTTON_NAMES[i]
-        if (self.joystick.get_button(8) == 1):
-            return BUTTON_NAMES[4]
+                return BUTTON_NUMBER_NAMES[i]
+
         hat = self.joystick.get_hat(0)[1]
-        if hat == 1: return BUTTON_NAMES[5]
-        elif hat == -1: return BUTTON_NAMES[6]
+        if hat != 0: return HAT_NUMBER_NAMES[hat]
         return None
 
-    def toggle_button(self, button_index):
-        """
-        Bytter tilstanden til en knapp med angitt indeksnummer `button_index`.
-        Returnerer navnet på knappen som ble endret.
-        """
-        button_name = BUTTON_NAMES[button_index]
-        self.active_buttons[button_name] = not self.active_buttons[button_name]
-        return button_name
     
     def apply_deadzone(self, value):
         """
