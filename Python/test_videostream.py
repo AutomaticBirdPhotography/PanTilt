7a733cf83b8f170fceeef3fb6bca77fec816f4c4
Robert Brenner Marthins <04roberten@gmail.com>
2023-06-19 21:54:41 +0200

    disse testene testet egentlig ingen ting

--- Python/test_videostream.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from vidTransfer import VideoStream
-
-class TestVideoStream:
-    def test_sendFrame(self):
-        videostream = VideoStream(logging=False, clientAddress="192.168.4.4", port="5454", framePercentage=20)
-
-        videostream.sendFrame(frame)
